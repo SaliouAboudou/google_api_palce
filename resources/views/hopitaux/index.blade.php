e55e158d6d2b312c34bd05db851aff03e808ab44
SaliouAboudou <105776499+SaliouAboudou@users.noreply.github.com>
2025-05-14 20:45:11 +0100

    add

--- resources/views/hopitaux/index.blade.php
+++ resources/views/hopitaux/index.blade.php
@@ -1,38 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Liste des Hôpitaux du Bénin</title>
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 20px;
-        }
-
-        th,
-        td {
-            padding: 8px 12px;
-            border: 1px solid #ccc;
-        }
-
-        th {
-            background-color: #f5f5f5;
-        }
-
-        h1 {
-            text-align: center;
-            margin-top: 20px;
-        }
-    </style>
-    <!-- Bootstrap 5 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-..." crossorigin="anonymous">
-
-</head>
-
-<body>
-    <h1>Hôpitaux du Bénin</h1>
+@extends('hopitaux.app')
+@section('content')
+    <h1>Recherche</h1>
     <form class=" row" action="{{ route('hopitaux') }}" method="GET">
         @csrf
         <div class="col-md-4">
@@ -109,6 +77,4 @@
             @endforelse
         </tbody>
     </table>
-</body>
-
-</html>
+@endsection
